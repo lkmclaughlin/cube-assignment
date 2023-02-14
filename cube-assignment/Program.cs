8a5d22cd79c051ae4677d091c4ce387c9e2796e2
Lynn M <lkfmclaugh@gmail.com>
2023-02-14 10:24:34 -0500

    Add project files.

---
+++ cube-assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Using if while
+//display the cube of the odd numbers from 1 to 25 (inclusive) using a WHILE loop
+
+int idx = 1;
+
+while(idx <= 25)
+{
+    if(idx % 2 == 0)
+    {
+        idx++; //still need to increment it.
+        continue; //skip it
+    }
+    //the number is odd
+    Console.WriteLine($"{idx}^3 is {idx * idx * idx}");
+
+    idx++; //don't forget this!
+}
+Console.WriteLine("Done");
